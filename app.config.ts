75c0007e26082aea53f088298ec33c1bbaa7d80b
Timothy Moore <mtimothy984@gmail.com>
2024-03-27 08:57:29 -0700

    1.3.8

--- app.config.ts
+++ app.config.ts
@@ -2,7 +2,7 @@ module.exports = {
   expo: {
     name: 'Oseh',
     slug: 'frontend-app',
-    version: '1.3.7',
+    version: '1.3.8',
     orientation: 'portrait',
     icon: './assets/icon.png',
     userInterfaceStyle: 'light',
@@ -33,7 +33,7 @@ module.exports = {
     android: {
       package: 'com.oseh.frontendapp',
       googleServicesFile: './google-services.json',
-      versionCode: 37 /* 1.3.7 */,
+      versionCode: 38 /* 1.3.8 */,
       adaptiveIcon: {
         foregroundImage: './assets/adaptive-icon.png',
         backgroundColor: '#000000',
