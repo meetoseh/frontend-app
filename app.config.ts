8d4b566199caa4cf5b629b262159150f7d14da89
Timothy Moore <mtimothy984@gmail.com>
2024-03-30 07:07:36 -0700

    update version to 1.4.5

--- app.config.ts
+++ app.config.ts
@@ -2,7 +2,7 @@ module.exports = {
   expo: {
     name: 'Oseh',
     slug: 'frontend-app',
-    version: '1.4.4',
+    version: '1.4.5',
     orientation: 'portrait',
     icon: './assets/icon.png',
     userInterfaceStyle: 'light',
@@ -33,7 +33,7 @@ module.exports = {
     android: {
       package: 'com.oseh.frontendapp',
       googleServicesFile: './google-services.json',
-      versionCode: 44 /* 1.4.4 */,
+      versionCode: 45 /* 1.4.5 */,
       adaptiveIcon: {
         foregroundImage: './assets/adaptive-icon.png',
         backgroundColor: '#000000',
