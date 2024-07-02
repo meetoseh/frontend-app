6fb88625b72aec14d1329e82601b0c03fe5b4a3a
Timothy Moore <mtimothy984@gmail.com>
2024-07-02 09:08:26 -0700

    1.6.6 - no changes
    bumping version to see if I can get google play billing lab to work with new pixel 8,
    which I think might need a test track build

--- app.config.ts
+++ app.config.ts
@@ -2,7 +2,7 @@ module.exports = {
   expo: {
     name: 'Oseh',
     slug: 'frontend-app',
-    version: '1.6.5',
+    version: '1.6.6',
     orientation: 'portrait',
     icon: './assets/icon.png',
     userInterfaceStyle: 'light',
@@ -33,7 +33,7 @@ module.exports = {
     android: {
       package: 'com.oseh.frontendapp',
       googleServicesFile: './google-services.json',
-      versionCode: 65 /* 1.6.5 */,
+      versionCode: 66 /* 1.6.6 */,
       adaptiveIcon: {
         foregroundImage: './assets/adaptive-icon.png',
         backgroundColor: '#000000',
