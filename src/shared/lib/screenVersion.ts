41b47d98097669d0470fd7fad55f8844fff170e3
Timothy Moore <mtimothy984@gmail.com>
2024-07-25 06:30:55 -0700

    v1.6.9

--- src/shared/lib/screenVersion.ts
+++ src/shared/lib/screenVersion.ts
@@ -3,4 +3,4 @@
  * to be equivalent to. This is provided to all peek / pop related requests,
  * and the backend handles mapping it to the correct screens.
  */
-export const SCREEN_VERSION = 68;
+export const SCREEN_VERSION = 69; /* 1.6.9 */
